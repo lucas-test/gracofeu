d2cbb1d98606405baa1432ec14efe0b9977537e9
YplN <thiebaut.jocelyn@gmail.com>
2022-07-14 10:57:40 +0200

    fixing parametor on area not correctly updating when resizing the area

--- src/public/parametors/parametor_manager.ts
+++ src/public/parametors/parametor_manager.ts
@@ -18,14 +18,14 @@ export function setup_parametors_available() {
 
 
 export function load_param(param: Parametor, canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, g: Graph, a: Area) {
-    const id =  param.id + "_area_" +( a==null?"null":a.id);
+    const html_id =  param.id + "_area_" +( a==null?"null":a.id);
     const area_id = (a==null?null:a.id);
 
     // console.log("ID", id, "AREA_ID", area_id);
 
     let newDiv = document.createElement("div");
     newDiv.classList.add("param");
-    newDiv.id = "param_" + id;
+    newDiv.id = "param_" + html_id;
 
 
     let button = document.createElement('div');
@@ -40,14 +40,14 @@ export function load_param(param: Parametor, canvas: HTMLCanvasElement, ctx: Can
     newDiv.appendChild(span_name);
 
     let span_result = document.createElement("span");
-    span_result.id = "span_result_" + id;
+    span_result.id = "span_result_" + html_id;
     console.log("SPAN ID", span_result.id);
     span_result.innerHTML = "";
     span_result.classList.add("result_span");
     newDiv.appendChild(span_result);
 
     document.getElementById("params_loaded").appendChild(newDiv);
-    params_loaded.push({parametor:param, area:a, id:id, area_id : area_id})
+    params_loaded.push({parametor:param, html_id:html_id, area_id : area_id})
     update_params_loaded(g)
     requestAnimationFrame(function () { draw(canvas, ctx, g) })
 }
@@ -58,24 +58,35 @@ export function load_param(param: Parametor, canvas: HTMLCanvasElement, ctx: Can
 export function update_params_loaded(g:Graph) {
     for (let param of params_loaded) {
         // console.log(param, param.parametor, param.area);
-        if(param.area === null){
+        if(param.area_id === null){
             var result = param.parametor.compute(g);
+            document.getElementById("span_result_" + param.html_id).innerHTML = result;
         }
         else{
-            var result = param.parametor.compute(param.area.get_subgraph(g));
+            if(g.areas.has(param.area_id)){
+                const area = g.areas.get(param.area_id);
+                var result = param.parametor.compute(area.get_subgraph(g));
+                document.getElementById("span_result_" + param.html_id).innerHTML = result;
+            }
+            else{
+                remove_loaded_param(param.html_id, param.area_id);
+            }
         }
-        document.getElementById("span_result_" + param.id).innerHTML = result;
     }
 }
 
 
 function remove_loaded_param(param_id: string, area_id:string) {
     for (var i = 0; i < params_loaded.length; i++) {
-        // console.log(params_loaded[i]);
-        if (params_loaded[i].parametor.id == param_id && area_id == params_loaded[i].area_id) {
-            document.getElementById("param_" + params_loaded[i].id).remove()
-            params_loaded.splice(i, 1)
-            return
+        if (params_loaded[i].parametor.html_id == param_id && area_id == params_loaded[i].area_id) {
+
+            const DOM = document.getElementById("param_" + params_loaded[i].html_id);
+            
+            if(DOM !== null){
+                DOM.remove()
+                params_loaded.splice(i, 1)
+                return
+            }
         }
     }
 }
